b0685395d6f9c60cc721a234a6f77e084b8a740e
hanzigk <374063924@qq.com>
2020-06-09 00:10:11 -0700

    update

--- dubbo-spring-study-master/dubbo-provider/src/main/java/com/minbo/dubbo/provider/DemoServiceImpl.java
+++ dubbo-spring-study-master/dubbo-provider/src/main/java/com/minbo/dubbo/provider/DemoServiceImpl.java
@@ -1,8 +1,11 @@
 package com.minbo.dubbo.provider;
 
 public class DemoServiceImpl implements DemoService{
+	public int count = 0;
 
 	public String sayHello(String name) {
+		System.out.println(count);
+		count++;
 		return "Welcome to Minbo's Dubbo, Hello " + name;
 	}
 
