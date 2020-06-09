98c0df3b1abd71aeb03edc2686f1147b1d65b120
EverlastingHopeX <491032242@qq.com>
2020-06-08 22:55:56 -0700

    simple load balance

--- dubbo-spring-study-master/dubbo-provider/src/main/java/com/balance/SimpleLoadBalance.java
+++ dubbo-spring-study-master/dubbo-provider/src/main/java/com/balance/SimpleLoadBalance.java
@@ -1,6 +1,7 @@
 package com.balance;
 
 import java.util.List;
+import java.util.Random;
 
 import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.rpc.Invocation;
@@ -8,12 +9,13 @@ import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.RpcException;
 import com.alibaba.dubbo.rpc.cluster.LoadBalance;
 
-public class LatencyLoadBalance implements LoadBalance {
+public class SimpleLoadBalance implements LoadBalance {
 
 	public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
 		// TODO Auto-generated method stub
-		
-		return null;
+		Random r=new Random();
+		int ran=r.nextInt(invokers.size());
+		return invokers.get(ran);
 	}
 
 }
